3d94c643aef67595f67ba5def1f4f76ba01ae9cd
raquelMoya <rakelms90@gmail.com>
2022-03-07 13:54:19 +0100

    Añadido redux para login/logout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-
 import './App.css';
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Containers/Home/Home';
@@ -9,19 +10,20 @@ import Login from './Containers/Login/Login';
 function App() {
   return (
     <div className="App">
-     
-     <BrowserRouter>
-     
-      <Header/>
 
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Login" element={<Login/>}/>
-      </Routes>
+      <BrowserRouter>
+      
+        <Header/>
 
-      <Footer/>
-     
-     </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+
+        <Footer/>
+      
+      </BrowserRouter>
+      
     </div>
   );
 }
